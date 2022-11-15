17ed683a8beb4350411cad2f8a450b359c7ced8e
Bruno Tiberio <quentemente@gmail.com>
2022-11-15 10:05:48 -0300

    feat: componente de resultado

---
+++ src/components/result/index.jsx
@@ -0,0 +1,25 @@
+export const Result = () => {
+  return (
+    <>
+      <h2>VOCÊ RECEBERÁ</h2>
+      <ul>
+        <li>
+            <p>Amanhã: </p>
+            <p>R$ 0,00</p>
+        </li>
+        <li>
+            <p>Em 15 dias: </p>
+            <p>R$ 0,00</p>
+        </li>
+        <li>
+            <p>Em 30 dias: </p>
+            <p>R$ 0,00</p>
+        </li>
+        <li>
+            <p>Em 90 dias: </p>
+            <p>R$ 0,00</p>
+        </li>
+      </ul>
+    </>
+  );
+};
