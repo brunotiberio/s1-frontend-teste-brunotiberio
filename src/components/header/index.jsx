2a7500c56efaaf06591ea69b76bfe1e71669473b
Bruno Tiberio <quentemente@gmail.com>
2022-11-15 06:44:38 -0300

    feat: header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./style"
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <h1>Simule sua antecipação</h1>
+        </StyledHeader>
+    )
+}
