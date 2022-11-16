8b24b065383130089f8a05dfe03eac9ec487bfad
Bruno Tiberio <quentemente@gmail.com>
2022-11-16 07:51:35 -0300

    feat: configuração react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ResultsProvider } from "./contexts/ResultsContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ResultsProvider>
-      <App />
-    </ResultsProvider>
+    <BrowserRouter>
+      <ResultsProvider>
+        <App />
+      </ResultsProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
