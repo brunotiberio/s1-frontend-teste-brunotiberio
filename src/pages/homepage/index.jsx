994efca6567cc01ecccc9edb849e0780ea8b7581
Bruno Tiberio <quentemente@gmail.com>
2022-11-16 07:52:03 -0300

    feat: criação das pages para rotas

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,11 @@
+import { Inputs } from "../../components/inputs";
+import { Result } from "../../components/result";
+
+export const Homepage = () => {
+  return (
+    <>
+      <Inputs />
+      <Result />
+    </>
+  );
+};
