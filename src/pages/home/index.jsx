b2f27629319e888dd3fc030fadd2080f36365b52
Bruno Tiberio <quentemente@gmail.com>
2022-11-17 14:31:09 -0300

    refact: adição do componente ResultLoading

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,27 +2,17 @@ import { Inputs } from "../../components/inputs";
 import { Result } from "../../components/result";
 import { useContext } from "react";
 import { ResultsContext } from "../../contexts/ResultsContext";
-import ContentLoader from "react-content-loader";
+import { StyledSectionMain } from "./style";
+import { ResultLoading } from "../../components/result-loading";
 
 export const Homepage = () => {
   const { data } = useContext(ResultsContext);
   return (
     <>
-      <Inputs />
-      {data ? (
-        <Result />
-      ) : (
-        <ContentLoader
-          speed={2}
-          width={400}
-          height={160}
-          viewBox="0 0 400 160"
-          backgroundColor="#f3f3f3"
-          foregroundColor="#ecebeb"
-        >
-          <rect x="-7" y="29" rx="3" ry="3" width="158" height="139" />
-        </ContentLoader>
-      )}
+      <StyledSectionMain>
+        <Inputs />
+        {data ? <Result /> : <ResultLoading />}
+      </StyledSectionMain>
     </>
   );
 };
